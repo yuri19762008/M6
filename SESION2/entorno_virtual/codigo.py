aea409cf88d7a8fb58b56df31fd90ed920799961
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-11-20 18:44:19 -0300

    fix

--- SESION2/entorno_virtual/codigo.py
+++
@@ -1,5 +0,0 @@
-print ("https://www.youtube.com/watch?v=W2LCF3YYpIY")
-
-## PS D:\0001 BOOT CAMP\BOOTCAMP\M6\sesion2\entorno_virtual\Scripts> .\activate
-##(entorno_virtual) PS D:\0001 BOOT CAMP\BOOTCAMP\M6\sesion2\entorno_virtual\Scripts> deactivate
-##PS D:\0001 BOOT CAMP\BOOTCAMP\M6\sesion2\entorno_virtual\Scripts> 
