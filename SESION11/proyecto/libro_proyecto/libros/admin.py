704d87770fc225a00c5e7c6cba7c3c31817da7ba
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-12-10 20:44:12 -0300

    fix

--- SESION11/proyecto/libro_proyecto/libros/admin.py
+++ SESION11/proyecto/libro_proyecto/libros/admin.py
@@ -1,16 +1,22 @@
-
-
-# Register your models here.
 from django.contrib import admin
 from .models import Libro
 
 class LibroAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'autor', 'valoracion', 'fecha_creacion', 'fecha_modificacion')
+    list_display = ('titulo', 'autor', 'valoracion', 'get_rating')
     search_fields = ('titulo', 'autor')
+    
+    def get_rating(self, obj):
+        if obj.valoracion < 1000:
+            return 'Baja'
+        elif 1000 <= obj.valoracion <= 2500:
+            return 'Media'
+        else:
+            return 'Alta'
+    
+    get_rating.short_description = 'Rating'
+    get_rating.admin_order_field = 'valoracion'
 
 admin.site.register(Libro, LibroAdmin)
-
-# Personalizar títulos del admin
 admin.site.site_header = 'Administración de Libros'
 admin.site.index_title = 'Panel de Control'
 admin.site.site_title = 'Biblioteca Django'
